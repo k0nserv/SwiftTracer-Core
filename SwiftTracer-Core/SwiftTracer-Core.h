fcf74b23a21fb1710d65216e755f3d4caff6e2a8
Hugo Tunius <hugo.tunius@gmail.com>
2016-01-18 22:15:21 +0000

    Create Xcode project

---
+++ SwiftTracer-Core/SwiftTracer-Core.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTracer-Core.h
+//  SwiftTracer-Core
+//
+//  Created by Hugo Tunius on 18/01/16.
+//  Copyright © 2016 Hugo Tunius. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftTracer-Core.
+FOUNDATION_EXPORT double SwiftTracer_CoreVersionNumber;
+
+//! Project version string for SwiftTracer-Core.
+FOUNDATION_EXPORT const unsigned char SwiftTracer_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTracer_Core/PublicHeader.h>
+
+
